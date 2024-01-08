58957eaafde5b63ec78743e3224c6778fcb7d12f
Richard Mulu <richard.ndisya@strathmore.edu>
2024-01-08 10:43:09 +0300

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -34,7 +34,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionId: '1'
+      deliveryOptionId: "1",
     });
   }
 
@@ -51,6 +51,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
